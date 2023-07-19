853f03929777c156d69be0200c1e4e6613ff44e4
SMHRD <SMHRD@211.48.213.242>
2023-07-19 20:45:25 +0900

    오징어 1차 완성 - ㅎㅈ

--- MINI_SQ/src/controller/Mp3.java
+++ MINI_SQ/src/controller/Mp3.java
@@ -21,6 +21,13 @@ public class Mp3 {
 		Bgm b6 = new Bgm("뺨2", ".\\music\\뺨2.mp3");
 		Bgm b7 = new Bgm("딱지치기공유버전", ".\\music\\딱지치기공유버전.mp3");
 		Bgm b8 = new Bgm("무궁화총소리", ".\\music\\무궁화총소리.mp3");
+		Bgm b9 = new Bgm("지하철2", ".\\music\\지하철2.mp3");
+		Bgm b10 = new Bgm("발자국2", ".\\music\\발자국2.mp3");
+		Bgm b11 = new Bgm("승합차2", ".\\music\\승합차2.mp3");
+		Bgm b12 = new Bgm("무궁화멘트2", ".\\music\\무궁화멘트2.mp3");
+		Bgm b13 = new Bgm("달고나멘트2", ".\\music\\달고나멘트2.mp3");
+		Bgm b14 = new Bgm("징검다리멘트2", ".\\music\\징검다리멘트2.mp3");
+		Bgm b15 = new Bgm("환호", ".\\music\\환호.mp3");
 		
 		
 
@@ -32,6 +39,13 @@ public class Mp3 {
 		mlist.add(b6);
 		mlist.add(b7);
 		mlist.add(b8);
+		mlist.add(b9);
+		mlist.add(b10);
+		mlist.add(b11);
+		mlist.add(b12);
+		mlist.add(b13);
+		mlist.add(b14);
+		mlist.add(b15);
 	}
 
 	public Bgm fx(int index) {
